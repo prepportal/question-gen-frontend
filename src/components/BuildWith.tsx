c8143f470436cce83be8bacbf1d8f716eea6fe2d
Akash R Chandran <chandranrakash@gmail.com>
2024-04-27 00:30:48 +0530

    animted logo to scroll horizontal infinetly

--- src/components/BuildWith.tsx
+++ src/components/BuildWith.tsx
@@ -1,53 +1,132 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+
+import { Poppins } from "next/font/google";
+const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 
 const BuildWith = () => {
   return (
-    <div className="my-5">
-      <div className="my-2 font-semibold">
-        <h3 className="text-base font-semibold justify-self-center">Built with</h3>
-        <div className="grid grid-cols-8 mx-auto">
-          <div className="flex gap-2">
+    <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+      <h2
+        className={`${cn(
+          "my-[6rem]",
+          poppins.className
+        )}}my-6 text-5xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-yellow-400`}
+      >
+        Build on Tech Stack
+      </h2>
+      <div className="w-full inline-flex flex-nowrap">
+        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+          <li>
             <Image
               alt="MySQL Azure"
               src="/mysql.svg"
-              width={130} height={130}
+              width={130}
+              height={130}
             />
-          </div>
-          <div className="flex items-center gap-2">
+          </li>
+          <li>
             <Image alt="nextjs" src="/nextjs.png" width={130} height={130} />
-          </div>
-          <div className="flex items-center gap-2">
-            <Image alt="tailwind" src="/tailwind.png" width={130} height={130} />
-          </div>
-          <div className="flex items-center gap-2">
-            <Image alt="nextauth" src="/nextauth.png" width={120} height={120} />
-          </div>
-          <div className="flex items-center gap-2">
-            <Image alt="openai" src="/openai.png" width={120} height={120} />
-          </div>
+          </li>
 
-          <div className="flex items-center gap-2">
+          <li>
+            <Image
+              alt="tailwind"
+              src="/tailwind.png"
+              width={130}
+              height={130}
+            />
+          </li>
+          <li className="flex items-center gap-2">
+            <Image
+              alt="nextauth"
+              src="/nextauth.png"
+              width={120}
+              height={120}
+            />
+          </li>
+          <li className="flex items-center gap-2">
+            <Image alt="openai" src="/openai.png" width={120} height={120} />
+          </li>
+
+          <li className="flex items-center gap-2">
             <Image
               alt="react query"
               src="/react-query.png"
               width={120}
               height={120}
             />
-          </div>
-          <div className="flex items-center gap-2">
+          </li>
+          <li className="flex items-center gap-2">
             <Image alt="primsa" src="/prisma.png" width={120} height={120} />
-          </div>
-          <div className="flex items-center gap-2">
+          </li>
+          <li className="flex items-center gap-2">
             <Image
               alt="typescript"
               src="/typescript.png"
               width={120}
               height={120}
             />
-          </div>
-        </div>
+          </li>
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden={true}
+        >
+          <li>
+            <Image
+              alt="MySQL Azure"
+              src="/mysql.svg"
+              width={130}
+              height={130}
+            />
+          </li>
+          <li>
+            <Image alt="nextjs" src="/nextjs.png" width={130} height={130} />
+          </li>
+
+          <li>
+            <Image
+              alt="tailwind"
+              src="/tailwind.png"
+              width={130}
+              height={130}
+            />
+          </li>
+          <li className="flex items-center gap-2">
+            <Image
+              alt="nextauth"
+              src="/nextauth.png"
+              width={120}
+              height={120}
+            />
+          </li>
+          <li className="flex items-center gap-2">
+            <Image alt="openai" src="/openai.png" width={120} height={120} />
+          </li>
+
+          <li className="flex items-center gap-2">
+            <Image
+              alt="react query"
+              src="/react-query.png"
+              width={120}
+              height={120}
+            />
+          </li>
+          <li className="flex items-center gap-2">
+            <Image alt="primsa" src="/prisma.png" width={120} height={120} />
+          </li>
+          <li className="flex items-center gap-2">
+            <Image
+              alt="typescript"
+              src="/typescript.png"
+              width={120}
+              height={120}
+            />
+          </li>
+        </ul>
       </div>
     </div>
   );
